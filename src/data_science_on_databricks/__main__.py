cec2f659e990099ee1e2f355d4b6a08669e1f92c
Jos van Nijnatten <9002122+vanNijnatten@users.noreply.github.com>
2025-12-25 21:41:24 +0100

    a good first setup w/ databricks & dab

--- src/data_science_on_databricks/__main__.py
+++ src/data_science_on_databricks/__main__.py
@@ -1,5 +1,20 @@
+import argparse
+
+from databricks.sdk.runtime import spark
+
+
 def main() -> None:
-    pass
+    parser = argparse.ArgumentParser(
+        description="Databricks job with catalog and schema parameters",
+    )
+    parser.add_argument("--catalog", required=True)
+    parser.add_argument("--schema", required=True)
+
+    args = parser.parse_args()
+
+    # Set the default catalog and schema
+    spark.sql(f"USE CATALOG {args.catalog}")
+    spark.sql(f"USE SCHEMA {args.schema}")
 
 
 if __name__ == "__main__":
--- tests/test_databricks.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.databricks
-@pytest.mark.parametrize("to_pass", [(True), (True)])
-def test_passes(to_pass: bool) -> None:
-    assert to_pass
