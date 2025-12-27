826dccdffb9a9064d4177b35a790647a97882d78
Jos van Nijnatten <9002122+vanNijnatten@users.noreply.github.com>
2025-12-27 11:29:27 +0100

    add conftest & an actual databricks/pyspark test

---
+++ tests/data_science_on_databricks/test_databricks.py
@@ -0,0 +1,20 @@
+import pytest
+from databricks.sdk.runtime import spark
+from pyspark.sql import DataFrame
+from pyspark.sql import types as T
+
+
+@pytest.mark.databricks
+def test_create_table() -> None:
+    data = [
+        (1,),
+        (2,),
+        (3,),
+    ]
+    schema = T.StructType(
+        [
+            T.StructField("index", T.StringType(), True),
+        ]
+    )
+    df: DataFrame = spark.createDataFrame(data, schema)  # pyright: ignore[reportUnknownMemberType]
+    assert df.count() == 3
